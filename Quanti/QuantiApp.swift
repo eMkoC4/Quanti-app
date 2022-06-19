ab8f30fcf342fbb33725101ed35ed8b51a230d9d
Ivan Svoboda <ivansvoboda9@gmail.com>
2022-06-19 20:08:17 +0200

    Initial Commit

---
+++ Quanti/QuantiApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuantiApp.swift
+//  Quanti
+//
+//  Created by IvanQ on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct QuantiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
